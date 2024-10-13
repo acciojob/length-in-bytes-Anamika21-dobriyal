793986886e391477da3f8ad192338bed45eaa1ff
Anamika <anamikadobriyal21@gmail.com>
2024-10-13 23:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const byteSize = (str) => {
   // write your code here
-};
+	function byteSize(str) {
+  // Create a Blob from the input string
+  const blob = new Blob([str]);
+  // Return the size of the Blob, which is the size of the string in bytes
+  return blob.size;
+}
 
+
+};
+ 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
